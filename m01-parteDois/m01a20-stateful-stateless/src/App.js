5818d2df98031c2b1587ce47f24add4bd8146781
Tiago Satur <tiagosatur@gmail.com>
2018-03-23 18:55:09 -0300

    Troca cores quadrados funcionando

--- m01-parteDois/m01a20-stateful-stateless/src/App.js
+++ m01-parteDois/m01a20-stateful-stateless/src/App.js
@@ -10,30 +10,27 @@ class App extends Component {
 		this.state = {
 			color: 'green'
 		}
+		this.changeColor = this.changeColor.bind(this)
 	}
 
-	handleClick(buttonColor) {
-		this.setState({color: '#000'})
+	changeColor(buttonColor) {
+		this.setState({ color: buttonColor })
 	}
+
 	// Como o nome da propriedade é o mesmo nome da variavel color, posso usar só color ES6
+	//Tarefa
+	// Botões que trocam svg e mude as cores do svg
+	// Autobind - https://babeljs.io/docs/plugins/transform-class-properties/
 	render() {
 		return (
 			<div className="container">
 				<Square color={this.state.color} />
 
-				{['red', 'green', 'blue'].map((color) => (
-					<Button
-						key={color}
-						onClick={ (e) => this.setState( {color})}>
+				{['red', 'green', 'blue'].map((color, index) => (
+					<Button key={index} changeColor={this.changeColor} color={color}>
 						{color}
 					</Button>
 				))}
-
-
-					<Button onClick={(e) => this.handleClick('pink')}
-					>
-					Pink
-					</Button>
 			</div>
 		)
 	}
