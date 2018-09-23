d618429227c25eca6145436a84fb86d456cf3abf
Tiago Satur <tiagosatur@gmail.com>
2018-09-23 20:44:02 -0300

    Intro to forms, input text, checkbox and radio

---
+++ m01-03-parte-02/m01a28-formsIntro/src/App.js
@@ -0,0 +1,124 @@
+'use strict'
+import React, { Component } from 'react'
+import './style/main.sass'
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			value: 'Valor inicial',
+			checked: false,
+			radioValue: 'Valor do radio'
+		}
+	}
+	render() {
+		return (
+			<div>
+				<h1>Intro to Forms</h1>
+				<h2>Input text</h2>
+				<form action="">
+					{/* Não é possível editar o value inicial de um input porque qualquer
+						componente react será renderizado e manterá aquele estado na forma
+						como foi gerado inicialmente. Para modificar esse valor vc deve
+						utilizar o método onchange, e num componente statefull, usar o estado
+						da aplicação pra modificar o valor do input.
+					*/}
+					<input type="text" value='Valor inicial'/>
+
+
+					{/*
+						O evento e do react é um evento sintético. Ele tem propriedades
+						diferentes do evento padrão do javascript. Dentre os eventos sintéticos
+						há uma propriedade chamada nativeEvent, o qual é o evento nativo
+						do próprio elmento Html. Essa forma de modificar inputs é chamada
+						CONTROLED COMPONENTS. São geralmente componentes stateless, que não
+						mantêm um estado próprio quando utilizada a propriedade value.
+						Se criarmos o input sem a prop value, ele fica editável. Isso acontece
+						pq ele se torna um componente statefull, mantendo um estado interno próprio,
+						para poder gerenciar as inserções de caracteres, mas não modifica o value.
+						Nesse caso, seria um UNCONTROLED COMPONENT.
+					*/}
+					<input type="text" value={this.state.value} onChange={(e) => {
+							console.log('Evento do react --- ', e);
+
+							this.setState({
+								value: e.target.value,
+							})
+						}}
+					/>
+
+
+					{/*
+						Caso vc queira utilizar um uncontroled component e mesmo assim passar
+						um valor inicial pra ele é possível usar o defaultValue. Ele será statefull.
+					*/}
+					<input type="text" defaultValue='Valor inicial'/>
+
+				</form>
+
+
+				<h2>Checkbox</h2>
+				<form action="">
+					<h3>Controlled checkbox</h3>
+					<label>
+						Check me
+						<input
+							type="checkbox"
+							value='my-chackbox'
+							checked={this.state.checked}
+							onChange={(e) => {
+								this.setState({
+									//As duas formas funcionam
+									checked: !this.state.checked
+									//checked: e.target.checked //Se negar, vc estará voltando ao estado anterior
+								})
+							}}
+						/>
+					</label>
+
+					<h3>Uncontrolled checkbox</h3>
+					<label>
+						Check me
+						<input
+							type="checkbox"
+							value='my-checkbox'
+							defaultChecked={false} // quando for true, naõ é preciso passar valor algum, somente o defaultChecked
+						/>
+					</label>
+				</form>
+
+				<h2>Input Radio</h2>
+				<form action="">
+					<h3>Controlled radio</h3>
+					<label>
+						Choose me
+						<input
+							type="radio"
+							name='myradio'
+							value={this.state.radioValue}
+							onChange={(e) => {
+								this.setState({
+									radioValue: e.target.value
+								})
+							}}
+						/>
+					</label>
+
+					<h3>Uncontrolled Radio</h3>
+					<label>
+						Choose me
+						<input
+							type="radio"
+							name='myradio'
+							value='my-radio-value'
+							defaultChecked
+						/>
+					</label>
+
+				</form>
+			</div>
+		)
+	}
+}
+
+export default App
