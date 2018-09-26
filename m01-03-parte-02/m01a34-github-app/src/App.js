8353e2d635e44fdb69f2d7352da3b10e2230b349
Tiago Satur <tiagosatur@gmail.com>
2018-09-26 01:19:31 -0300

    Add sass to react app and style the page

--- m01-03-parte-02/m01a34-github-app/src/App.js
+++ m01-03-parte-02/m01a34-github-app/src/App.js
@@ -9,20 +9,38 @@ class App extends Component {
 
           <div className='user-info'>
             {/* <img src="https://avatars1.githubusercontent.com/u/24196412?v=4" alt="Avatar"/> */}
-            <img src="https://dummyimage.com/400x400/ffffff/000000" alt="Avatar"/>
-            <h2>
-              <a href="https://github.com/tiagosatur">Tiago Satur</a>
-            </h2>
-            <ul className='repos-info'>
-              <li>Repositórios: 22</li>
-              <li>Seguidores: 6</li>
-              <li>Seguindo: 13</li>
-            </ul>
+            <div className='search p-01 mb-02'>
+              <input type="text" placeholder='O que você procura?'/>
+            </div>
 
-            <div className='actions'>
+
+
+            <div className='main d-flex justify-content-between p-01'>
+              <div className='avatar'>
+                <img src="https://dummyimage.com/200x200/ffffff/000000" alt="Avatar"/>
+              </div>
+
+              <div className='user-infos'>
+                <h2>
+                  <a href="https://github.com/tiagosatur">Tiago Satur</a>
+                </h2>
+
+                <ul className='repos-info'>
+                  <li>Repositórios: 22</li>
+                  <li>Seguidores: 6</li>
+                  <li>Seguindo: 13</li>
+                </ul>
+              </div>
+              </div>
+
+
+            <div className='actions d-flex justify-content-between p-01'>
               <button>Ver repositórios</button>
               <button>Ver favoritos</button>
             </div>
+
+
+
 
             <div className='repos'>
               <h2>Repositórios:</h2>
