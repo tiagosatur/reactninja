99668f9c627c3731889730f407a4b7f60ffa6323
Tiago Satur <tiagosatur@gmail.com>
2018-09-27 21:56:02 -0300

    Componentize the application, and pass state to them via props

--- m01-03-parte-02/m01a34-github-app/src/components/Search.js
+++ m01-03-parte-02/m01a34-github-app/src/components/Search.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Search = () => (
-  <div className='search p-01 mb-02'>
+  <div className='search p-05 mb-02'>
     <input
       type="text"
       placeholder='Qual usuário do GitHub você procura?'/>
