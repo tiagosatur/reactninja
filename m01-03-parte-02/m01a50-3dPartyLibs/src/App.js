7364c563bb28cf43db19ec8f6feb68a896934b0f
Tiago Satur <tiagosatur@gmail.com>
2018-10-09 20:09:21 -0300

    Ref property

--- m01-03-parte-02/m01a50-3dPartyLibs/src/App.js
+++ m01-03-parte-02/m01a50-3dPartyLibs/src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import ajax from '@fdaciuk/ajax'
+import Plugin from './components/Plugin/Plugin'
 import './style/App.css';
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      state: false,
-
-    }
-	}
+  /*
+    Como utilizamos bibliotecas de terceiros junto com react?
+    Já que com react, não precisamos nos preocupar com manipulação de DOM,
+    mas somente com manipulação de dados.
+  */
 
   render() {
     return (
-      <div>
-        <h3>Third party libs with React</h3>
+      <div className='container' style={{display: 'flex', flexDirection:'column', justifyContent: 'center'}}>
+        <h3 style={{display: 'flex', justifyContent: 'center'}}>Third party libs with React</h3>
+
+        <Plugin />
       </div>
     );
   }
