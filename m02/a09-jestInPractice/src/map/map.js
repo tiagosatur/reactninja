493f3ea8c3eaf4c7c2fd3f6280f362609a3053ed
Tiago Satur <tiagosatur@gmail.com>
2018-10-25 20:56:37 -0300

    More jest

--- m02/a09-jestInPractice/src/map/map.js
+++ m02/a09-jestInPractice/src/map/map.js
@@ -1,8 +1,19 @@
-export default (arr, func) => {
+const map =  (arr = [], func) => {
+  if(typeof func !== 'function') {
+    throw new TypeError('func is not a function!')
+  }
+
+  if(!Array.isArray(arr)) {
+    throw new TypeError('arr is not an array!')
+  }
+  
   let newArr = []
 
   for(let i = 0; i < arr.length; i++) {
-    newArr.push(func(arr[i]))
+    newArr.push(func(arr[i]), i, arr)
   }
 
+  return newArr;
 }
+
+export default map;
