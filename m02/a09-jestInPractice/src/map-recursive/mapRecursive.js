d698bdc0be41f4113af5a78301612b654b24ac22
Tiago Satur <tiagosatur@gmail.com>
2018-11-18 23:02:22 -0200

    map recursive + refactoring

--- m02/a09-jestInPractice/src/map-recursive/mapRecursive.js
+++ m02/a09-jestInPractice/src/map-recursive/mapRecursive.js
@@ -1,10 +1,34 @@
-const mapRecursive =  (arr = [], func = (item) => item) => {
-  // Forma imperativa
-  if(arr.length === 0)
-    return []
+// const mapRecursive =  (arr = [], func = (item) => item) => {
+//   // Forma funcional
+//
+//   let counter = 0
+//
+//   const mapInternal = (arrayInternal) => {
+//     if(arrayInternal.length === 0)
+//       return []
+//
+//     const [head, ...tail] = arrayInternal
+//     return [func(head, counter++, arr)].concat(mapInternal(tail, func))
+//   }
+//
+//   return mapInternal(arr)
+//
+// }
 
-    const [head, ...tail] = arr
-    return[func(head)].concat(map(tail, func))
+//Refactoring
+
+const mapRecursive =  (arr = [], func = (item) => item) => {
+  // Forma funcional
+
+  // IIFFE
+  return (function mapInternal(arrayInternal, counter) {
+    const [head, ...tail] = arrayInternal
+    return arrayInternal.length === 0 ? [] : [
+      func(head, counter, arr),
+      ...mapInternal(tail, counter + 1)
+    ]
+  })(arr, 0)
+
 }
 
 export default mapRecursive;
