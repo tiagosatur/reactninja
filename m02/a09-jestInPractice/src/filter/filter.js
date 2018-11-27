5940d784cc94ecc168a9593199640dfab841916f
Tiago Satur <tiagosatur@gmail.com>
2018-11-27 10:19:54 -0200

    filter function

--- m02/a09-jestInPractice/src/filter/filter.js
+++ m02/a09-jestInPractice/src/filter/filter.js
@@ -1,5 +1,15 @@
-const filter = () => {
-  return [1, 2, 3]
+const filter = (arr, func) => {
+  let newArr = []
+
+  for (let i = 0; i < arr.length; i++) {
+    //Verify if the item passed to the function returns true
+    //if true push into the array
+    if(func(arr[i], i, arr)) {
+      newArr.push(arr[i])
+    }
+  }
+
+  return newArr
 }
 
 
